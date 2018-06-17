55336c04bd025b0d6d4ea030a44031cbb6d695f0
mimikim <mimikimwebdeveloper@gmail.com>
2018-06-17 05:04:31 -0400

    adding Starfield class, creates animated floating stars in canvas

--- app/includes/registerPartials.js
+++ app/includes/registerPartials.js
@@ -5,7 +5,7 @@ const hbs = require('hbs');
 const path = require('path');
 
 module.exports = function() {
-  hbs.registerPartial( 'head', fs.readFileSync(path.join(__dirname, '../views/head.hbs'), 'utf8') );
-  hbs.registerPartial( 'footer', fs.readFileSync(path.join(__dirname, '../views/footer.hbs'), 'utf8') );
-  hbs.registerPartial( 'portfolio-thumb', fs.readFileSync(path.join(__dirname, '../views/portfolio-thumb.hbs'), 'utf8') );
+  hbs.registerPartial( 'head', fs.readFileSync(path.join(__dirname, '../views/partial/head.hbs'), 'utf8') );
+  hbs.registerPartial( 'footer', fs.readFileSync(path.join(__dirname, '../views/partial/footer.hbs'), 'utf8') );
+  hbs.registerPartial( 'portfolio-thumb', fs.readFileSync(path.join(__dirname, '../views/partial/portfolio-thumb.hbs'), 'utf8') );
 };
--- app/src/js/modules/animation-starfield.js
+++ app/src/js/modules/animation-starfield.js
@@ -2,28 +2,148 @@
 
 export class Starfield {
   constructor() {
-    this.num = 80;
-    this.radius = 4;
+    this.stars = [];
+    this.options = {
+      num: 80,
+      minSize: 0.5,
+      maxSize: 4,
+      opacitySpeed: 0.005,
+      opacityMin: 0.01,
+      opacityMax: 0.6,
+      minSpeed: 0.01,
+      maxSpeed: 0.09
+    };
 
     this.init();
+
+    window.requestAnimationFrame( () => {
+      this.animate();
+    } );
+
+    window.addEventListener( 'resize', () => {
+      this.stars = [];
+      this.canvas.parentNode.removeChild( this.canvas );
+      this.init();
+    });
   }
 
   init() {
     this.createCanvas();
-    this.draw();
-
-    window.addEventListener('resize', () => {
-      // determine max num of stars to draw based off window height/width
-      this.num = 80;
-
-      this.draw();
-    });
+    this.setCanvasSize();
+    this.createStars();
   }
 
-  draw() {
-    this.setCanvasSize();
+  // generates array of stars
+  createStars() {
+    for ( let i = 0; i < this.options.num ; i++ ) {
+      let opacity = Math.random() * ( this.options.opacityMax - this.options.opacityMin ) + this.options.opacityMin;
+      let size = Math.random() * ( this.options.maxSize - this.options.minSize ) + this.options.minSize;
+
+      let xpos = Math.floor( Math.random() * ( window.innerWidth + 1 ) );
+      let ypos = Math.floor( Math.random() * ( window.innerHeight + 1 ) );
+
+      let xspeed = Math.random() * ( this.options.maxSpeed - this.options.minSpeed ) + this.options.minSpeed / 10;
+      let yspeed = Math.random() * ( this.options.maxSpeed - this.options.minSpeed ) + this.options.minSpeed / 10;
+
+      this.stars.push( {
+        opacity: opacity,
+        size: size,
+        xpos: xpos,
+        ypos: ypos,
+        xspeed: xspeed,
+        yspeed: yspeed,
+        reverseX: ( Math.floor( xspeed * 100 ) % 2 === 0 ),
+        reverseY: ( Math.floor( yspeed * 100 ) % 2 === 0 ),
+        reverseOpacity: false,
+        reverseSize: ( Math.floor( size * 100 ) % 2 === 0 )
+      } );
+    }
+  }
+
+  drawStars() {
+    for( let i = 0; i < this.stars.length; i++ ) {
+
+      // if direction x is getting to end of width
+      if ( this.stars[i].xpos >= ( window.innerWidth + 20 ) ) {
+        this.stars[i].reverseX = true;
+      }
+
+      // if direction x is getting close to 0
+      if ( this.stars[i].xpos <= -40 ) {
+        this.stars[i].reverseX = false;
+      }
+
+      // x position
+      if ( this.stars[i].reverseX ) {
+        this.stars[i].xpos -= this.stars[i].xspeed;
+      } else {
+        this.stars[i].xpos += this.stars[i].xspeed;
+      }
+
+      // if direction y is getting to end of width
+      if ( this.stars[i].ypos >= ( window.innerHeight + 20 ) ) {
+        this.stars[i].reverseY = true;
+      }
+
+      // if direction y is getting close to 0
+      if ( this.stars[i].ypos <= -40 ) {
+        this.stars[i].reverseY = false;
+      }
+
+      // y position
+      if ( this.stars[i].reverseY ) {
+        this.stars[i].ypos -= this.stars[i].yspeed;
+      } else {
+        this.stars[i].ypos += this.stars[i].yspeed;
+      }
+
+      // reverse opacity, if opacity is getting close to 0.6
+      if ( this.stars[i].opacity >= this.options.opacityMax ) {
+        this.stars[i].reverseOpacity = true;
+      }
+
+      // increment opacity, if opacity hits less than 0.02
+      if ( this.stars[i].opacity <= this.options.opacityMin ) {
+        this.stars[i].reverseOpacity = false;
+      }
+
+      if ( this.stars[i].reverseOpacity ) {
+        this.stars[i].opacity -= this.options.opacitySpeed;
+      } else {
+        this.stars[i].opacity += this.options.opacitySpeed;
+      }
+
+      // sizes
+      if ( this.stars[i].size >= this.options.maxSize ) {
+        this.stars[i].reverseSize = true;
+      }
+
+      if ( this.stars[i].size <= this.options.minSize ) {
+        this.stars[i].reverseSize = false;
+      }
+
+      if ( this.stars[i].reverseSize ) {
+        this.stars[i].size -= 0.04;
+      } else {
+        this.stars[i].size += 0.03;
+      }
+
+      this.context.beginPath();
+      this.context.arc( this.stars[i].xpos, this.stars[i].ypos, this.stars[i].size, 0, 2 * Math.PI );
+      this.context.fillStyle = 'rgba( 255, 255, 255, ' + this.stars[i].opacity + ' )';
+      this.context.fill();
+      this.context.closePath();
+    }
+  }
+  
+  animate() {
     this.clearCanvas();
-    this.createStars();
+    this.drawStars();
+
+    // request a new frame
+    window.requestAnimationFrame( () => {
+      this.animate();
+    } );
   }
 
   clearCanvas() {
@@ -45,45 +165,5 @@ export class Starfield {
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
   }
-
-  createStars() {
-    for ( let i = 0; i <= this.num; i++ ) {
-      let rand_x = Math.random() * window.innerWidth;
-      let rand_y = Math.random() * window.innerHeight;
-      let rand_radius = Math.random() * this.radius + 2;
-      let rand_opacity = Math.random() * 0.4;
-
-      this.context.beginPath();
-      this.context.arc( rand_x, rand_y, rand_radius, 0, 2 * Math.PI );
-      this.context.fillStyle = 'rgba( 255, 255, 255, ' + rand_opacity + ' )';
-      this.context.fill();
-      this.context.closePath();
-    }
-  }
-
-
-
-  generatePosition() {
-    var h = $(window).height();
-    var w = $(window).width();
-
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-
-    return [nh,nw];
-  }
-
-  animateDiv( elm ){
-    var newq = this.generatePosition();
-
-    $( elm ).animate(
-      { top: newq[0], left: newq[1] },
-      10000,
-
-      function() {
-        this.animateDiv( elm );
-      }
-    );
-  };
 
 }
