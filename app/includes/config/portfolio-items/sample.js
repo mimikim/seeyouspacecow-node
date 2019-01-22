4a5e3ecac953e854f26985387de3602f32668798
Mimi Kim <mimikim@users.noreply.github.com>
2019-01-22 05:08:48 -0500

    Adding sample Portfolio Item for demo rendering

---
+++ app/includes/config/portfolio-items/sample.js
@@ -0,0 +1,9 @@
+// sample portfolio item, just so that content example loads
+
+module.exports = {
+  name: 'Sample Portfolio Item',
+  url: 'sample-item',
+  thumb: '/img/harvestmoon-cow-tail-wag.png',
+  type: ['static'],
+  content: '<p>This is an example of a portfolio item</p>'
+};
