009428d7d7cd0fbeebf00c9106dcd87313f9ce41
Mimi Kim <mimikim@users.noreply.github.com>
2019-01-22 03:50:08 -0500

    creating sample portfolio item for repo

---
+++ app/includes/config/portfolio-items/sample.js
@@ -0,0 +1,9 @@
+// sample portfolio item, just so that content loads
+
+module.exports = {
+  name: 'Sample Portfolio Item',
+  url: 'sample-item',
+  thumb: '',
+  type: ['static'],
+  content: '<p>This is an example of a portfolio item</p>'
+};
